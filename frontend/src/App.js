9959c347cf27d6fb59b6d55544fbf1ee8c086dae
uk1996 <csuk1947@gmail.com>
2022-05-02 14:12:58 +0900

    Ant Design 적용해보기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
+import React from "react";
+import { Button } from "antd";
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Button type="primary" onClick={() => console.log("clicked")}>
+        Hello, Antd
+      </Button>
+    </div>
+  );
 }
 
 export default App;
