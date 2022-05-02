489f3c2f19c8fd49f8fef7469c1a1b2fcedf0588
uk1996 <csuk1947@gmail.com>
2022-05-02 15:44:01 +0900

    React 엘리먼트 사용해보기, .prettierrc 파일 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,49 @@
-import React from "react";
-import { Button } from "antd";
-import "./App.css";
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+class Counter1 extends React.Component {
+  state = {
+    value: this.props.initialValue,
+  };
+
+  onClick = () => {
+    const { value } = this.state;
+    this.setState({ value: value + 1 });
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <div>
+        Counter1: {value}
+        <Button onClick={this.onClick}>+1</Button>
+      </div>
+    );
+  }
+}
+
+class FruitComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>좋아하는 과일</h1>
+        <ul>
+          {this.props.fruits.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 function App() {
+  const fruits = ['바나나', '사과', '딸기'];
   return (
     <div>
-      <Button type="primary" onClick={() => console.log("clicked")}>
-        Hello, Antd
-      </Button>
+      <Counter1 initialValue={10}></Counter1>
+      <FruitComponent fruits={fruits}></FruitComponent>
     </div>
   );
 }
