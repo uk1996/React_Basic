ef57fc61cc014e2f75af15db9d8a75eeb5e513b1
uk1996 <csuk1947@gmail.com>
2022-05-03 10:26:12 +0900

    상태값 로직 컴포넌트에서 분리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,48 +2,37 @@ import React from 'react';
 import { Button } from 'antd';
 import './App.css';
 
-class Counter1 extends React.Component {
-  state = {
-    value: this.props.initialValue,
-  };
+const actions = {
+  init(initialValue) {
+    return { value: initialValue };
+  },
+  increment(prevState) {
+    return { value: prevState.value + 1 };
+  },
+  decrement(prevState) {
+    return { value: prevState.value - 1 };
+  },
+};
 
-  onClick = () => {
-    const { value } = this.state;
-    this.setState({ value: value + 1 });
-  };
+class Counter1 extends React.Component {
+  state = actions.init(this.props.initialValue);
 
   render() {
     const { value } = this.state;
     return (
       <div>
         Counter1: {value}
-        <Button onClick={this.onClick}>+1</Button>
-      </div>
-    );
-  }
-}
-
-class FruitComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>좋아하는 과일</h1>
-        <ul>
-          {this.props.fruits.map((name, index) => (
-            <li key={index}>{name}</li>
-          ))}
-        </ul>
+        <Button onClick={() => this.setState(actions.increment)}>+1</Button>
+        <Button onClick={() => this.setState(actions.decrement)}>-1</Button>
       </div>
     );
   }
 }
 
 function App() {
-  const fruits = ['바나나', '사과', '딸기'];
   return (
     <div>
       <Counter1 initialValue={10}></Counter1>
-      <FruitComponent fruits={fruits}></FruitComponent>
     </div>
   );
 }
