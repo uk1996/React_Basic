81c46ba4de5b976bde1c6e0b820de6ea4c619c2c
uk1996 <csuk1947@gmail.com>
2022-05-05 10:00:00 +0900

    css 적용하기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Counter from './Counter';
+import Messasge from './Message';
+import Profile from './Profile';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Messasge />
+        <Profile />
         <Counter />
         <Counter color="green" />
         <Counter color="blue" />
