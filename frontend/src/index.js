1d42189df9270b0b1a39a6ee281b6e736b309e55
uk1996 <csuk1947@gmail.com>
2022-05-10 10:00:17 +0900

    커스텀 Hook 만들기

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import App3 from './App3';
 import App4 from './App4';
+import App5 from './App5';
 import reportWebVitals from './reportWebVitals';
 import TodoList from './TodoList';
 import EpisodeList from './EpisodeList';
@@ -11,7 +12,7 @@ import EpisodeList from './EpisodeList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App4 />
+    <App5 />
   </React.StrictMode>,
 );
 
